879ffa31d7fe6b0409935dd8a876dd6c9d6a6617
Mathieu Bredif <mathieu.bredif@ign.fr>
2019-09-23 16:26:57 +0200

    gh-pages-deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export { THREE };
-export { default as MatisOrientationParser } from './parsers/MatisOrientationParser';
-export { default as MicmacOrientationParser } from './parsers/MicmacOrientationParser';
-export { default as PhotogrammetricCamera } from './cameras/PhotogrammetricCamera';
-export { default as FilesSource } from './sources/FilesSource';
-export { default as FetchSource } from './sources/FetchSource';
-export { default as OrientedImageMaterial } from './materials/OrientedImageMaterial';
